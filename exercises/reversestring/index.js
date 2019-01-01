e50c1ab671133fad8878792e77f524fa5e03409e
unknown <mani91g@gmail.com>
2019-01-01 21:43:30 +0530

    Section 3

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return  str.split('').reduce((a,b) => b+a, '' )
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+
+// function reverse(str) {
+//     var strResult = "";
+
+//     for( i= 0; i < str.length; i++)
+//     {
+//         strResult =  strResult + str[str.length - 1 - i]
+//     }
+
+//     return strResult;
+// }
+
+// function reverse(str) {
+//     var strResult = "";
+
+//     for( let c of str)
+//     {
+//         strResult =  c + strResult
+//     }
+
+//     return strResult;
+// }
